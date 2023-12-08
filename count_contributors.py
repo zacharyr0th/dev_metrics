20d1b84095e6eae2953399d214829537167be808
zacharyr0th <100426704+zacharyr0th@users.noreply.github.com>
2023-12-08 16:51:52 -0600

    added csv

--- count_contributors.py
+++ count_contributors.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from collections import defaultdict
 
-def count_contributors(commits):
+def count_contributors(commits, full_time_threshold=10):
     total_commits = len(commits)
     contribution_days = defaultdict(set)
 
@@ -9,21 +9,27 @@ def count_contributors(commits):
         try:
             author = commit.get('commit', {}).get('author', {}).get('name', 'Unknown')
             date_str = commit.get('commit', {}).get('author', {}).get('date', '')
-            date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').date()
-            contribution_days[author].add(date)
+
+            if date_str:
+                date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').date()
+                contribution_days[author].add(date)
+            else:
+                print(f"Warning: Missing date for commit by {author}")
+
         except Exception as e:
-            print(f"Error processing commit for contributor count: {e}")
+            print(f"Error processing commit for contributor count (Author: {author}): {e}")
 
     full_time_contributors = 0
     part_time_contributors = 0
     one_time_contributors = 0
 
     for days in contribution_days.values():
-        if len(days) >= 10:
+        days_count = len(days)
+        if days_count >= full_time_threshold:
             full_time_contributors += 1
-        elif 1 <= len(days) < 10:
+        elif 1 <= days_count < full_time_threshold:
             part_time_contributors += 1
-        elif len(days) == 1:
+        elif days_count == 1:
             one_time_contributors += 1
 
     return total_commits, full_time_contributors, part_time_contributors, one_time_contributors
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import toml
+import csv
 from data_fetch import fetch_repo_data
 from filter_commits import filter_commits
 from count_contributors import count_contributors
@@ -8,30 +9,67 @@ def main():
     with open('aptosRepos.toml', 'r') as file:
         data = toml.load(file)
 
+    # Extract repository URLs - limit to first 20
+    repo_urls = [repo['url'] for repo in data['repo']][:20]
+
+    # Open a file to save the results
+    with open('results.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        # Write the header row
+        csvwriter.writerow(['Repository', 'Total Commits', 'Full-Time Contributors', 'Part-Time Contributors', 'One-Time Contributors'])
+
+        # Iterate over each repository URL
+        for repo_url in repo_urls:
+            try:
+                # Fetch commit and contributor data
+                commits, contributors = fetch_repo_data(repo_url)
+
+                # Filter commits
+                filtered_commits = filter_commits(commits)
+
+                # Count contributors
+                total_commits, full_time, part_time, one_time = count_contributors(filtered_commits)
+
+                # Write the results to the CSV file
+                csvwriter.writerow([repo_url, len(filtered_commits), full_time, part_time, one_time])
+
+            except Exception as e:
+                print(f"Error processing repository {repo_url}: {e}")
+
+'''
+the following is to iterate through the entire aptosRepos.toml file - takes forever with GH's rate limiting
+
+def main():
+    # Load the TOML file
+    with open('aptosRepos.toml', 'r') as file:
+        data = toml.load(file)
+
     # Extract repository URLs
     repo_urls = [repo['url'] for repo in data['repo']]
 
-    # Iterate over each repository URL
-    for repo_url in repo_urls:
-        try:
-            # Fetch commit and contributor data
-            commits, contributors = fetch_repo_data(repo_url)
+    # Open a file to save the results
+    with open('results.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        # Write the header row
+        csvwriter.writerow(['Repository', 'Total Commits', 'Full-Time Contributors', 'Part-Time Contributors', 'One-Time Contributors'])
 
-            # Filter commits
-            filtered_commits = filter_commits(commits)
+        # Iterate over each repository URL
+        for repo_url in repo_urls:
+            try:
+                # Fetch commit and contributor data
+                commits, contributors = fetch_repo_data(repo_url)
 
-            # Count contributors
-            total_commits, full_time, part_time, one_time = count_contributors(filtered_commits)
+                # Filter commits
+                filtered_commits = filter_commits(commits)
 
-            # Output the results
-            print(f"Repository: {repo_url}")
-            print(f"Total Commits: {len(filtered_commits)}")
-            print(f"Full-Time Contributors: {full_time}")
-            print(f"Part-Time Contributors: {part_time}")
-            print(f"One-Time Contributors: {one_time}")
-            print("\n")
-        except Exception as e:
-            print(f"Error processing repository {repo_url}: {e}")
+                # Count contributors
+                total_commits, full_time, part_time, one_time = count_contributors(filtered_commits)
 
+                # Write the results to the CSV file
+                csvwriter.writerow([repo_url, len(filtered_commits), full_time, part_time, one_time])
+
+            except Exception as e:
+                print(f"Error processing repository {repo_url}: {e}")
+'''             
 if __name__ == "__main__":
     main()
